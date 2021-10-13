8773e76ac93fed9261c2d45a026fef3729b88346
Latrell <lll4xy@virginia.edu>
2021-10-13 14:10:39 -0400

    adding in code for rest client

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 const notes = [
     {
@@ -19,3 +23,42 @@ const notes = [
       important: true
     }
   ]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if(note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    
+})
+
+app.delete('api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
