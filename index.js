c47915614ee67f4709f2750480346579bd92b3b9
Latrell <lll4xy@virginia.edu>
2021-10-14 14:16:02 -0400

    adding production frontend build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
