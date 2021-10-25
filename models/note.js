6f3a3e6674cc5eefb0e8c13967b531d506043c24
Latrell <lll4xy@virginia.edu>
2021-10-25 17:05:54 -0400

    codealong part 4

--- models/note.js
+++ models/note.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log(result, 'connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
