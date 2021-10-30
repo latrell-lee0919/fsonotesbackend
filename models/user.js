51ae695668936e40785fb25909bd23dd06dd0a22
Latrell <lll4xy@virginia.edu>
2021-10-30 13:23:03 -0400

    part 4-8

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+//const uniqueValidator = require('mongoose-unique-validator')
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: {
     type: String,
     unique: true
@@ -16,16 +16,17 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
+//userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
+    // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
 })
-
-userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
