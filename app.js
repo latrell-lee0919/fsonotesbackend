9a9bef3db7c25eeff9267a09db9d84421aa62457
Latrell <lll4xy@virginia.edu>
2021-11-15 17:20:51 -0500

    set up test DB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
+require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
@@ -11,6 +12,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
+logger.info(process.env.NODE_ENV)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -28,6 +30,11 @@ app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
